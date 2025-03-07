2e6f1f19629d027b017ce21b620b3becc9de3192
raghunathreddymustur <38384536+raghunathreddymustur@users.noreply.github.com>
2025-03-07 12:10:38 +0530

    Add files via upload
    added test cases for newer requriements

---
+++ day3/DomainFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace hamaraBasket.Tests
+{
+    internal class DomainFactory
+    {
+        private static DomainFactory myInstance;
+
+        [SetUp]
+        public void Setup()
+        {
+            myInstance = new DomainFactory();
+        }
+
+        public static Item CreateItem(string name, int sellIn, int quality)
+        {
+            return new Item { Name = name, SellIn = sellIn, Quality = quality };
+        }
+
+        public static HamaraBasket CreateHamaraBasket(IList<Item> items)
+        {
+            return new HamaraBasket(items);
+        }
+    }
+}
---
+++ day3/HamaraBasketTest.cs
@@ -0,0 +1,118 @@
+﻿using static hamaraBasket.Tests.DomainFactory;
+
+
+namespace hamaraBasket
+{
+    [TestFixture]
+    public class HamaraBasketTest
+    {
+        [Test]
+        public void GeneralItem_QualityAndSellInDecreaseByOne()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("foo", 10, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].SellIn, Is.EqualTo(9));
+            Assert.That(Items[0].Quality, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void GeneralItem_QualityDoesNotGoBelowZero()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("foo", 10, 0) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GeneralItem_QualityDecreasesByTwoAfterSellInDate()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("foo", 0, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void IndianWine_QualityIncreasesByOne()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Indian Wine", 10, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void IndianWine_QualityDoesNotExceedFifty()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Indian Wine", 10, 50) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void IndianWine_QualityIncreasesByTwoAfterSellInDate()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Indian Wine", 0, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void MovieTickets_QualityIncreasesByOneWhenSellInGreaterThanTen()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Movie Tickets", 11, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void MovieTickets_QualityIncreasesByTwoWhenSellInTenOrLess()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Movie Tickets", 10, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void MovieTickets_QualityIncreasesByThreeWhenSellInFiveOrLess()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Movie Tickets", 5, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(13));
+        }
+
+        [Test]
+        public void MovieTickets_QualityDropsToZeroAfterSellInDate()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Movie Tickets", 0, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ForestHoney_QualityDoesNotChange()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Forest Honey", 10, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Quality, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void ForestHoney_SellInDoesNotChange()
+        {
+            IList<Item> Items = new List<Item> { CreateItem("Forest Honey", 10, 10) };
+            HamaraBasket app = CreateHamaraBasket(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].SellIn, Is.EqualTo(10));
+        }
+    }
+}
